420cdebf31022bb78c1ae040dac64a19b69b4953
shrirajthorat <shrirajthorat33@gmail.com>
2020-05-11 23:34:12 -1200

    basic js

---
+++ Java Script/Lockdown_work/Day1/jscode.js
@@ -0,0 +1,14 @@
+function heighest_no(a,b,c)
+{
+    document.getElementById("demo").innerHTML = Math.max(a,b,c);
+}
+function my_name()
+{
+    document.getElementById("name").innerHTML="Shriraj Thorat";
+    console.log("Shriraj Thorat  inside console");
+    alert("Shriraj Thorat");
+}
+function n()
+{
+    document.getElementById("nam").innerHTML="<h1> Shriraj </h1> <h2> Shriraj </h2> <h3> Shriraj </h3> <h4> Shriraj </h4> <h5> Shriraj </h5>" ;
+}
